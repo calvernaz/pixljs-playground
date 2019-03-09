c6f40c895268e845e8fb87535624f8500a67f599
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2019-03-09 10:35:21 +0000

    screen witdh + height

---
+++ screen-fun.js
@@ -0,0 +1,6 @@
+// Pixl.Js LCD display is double-buffered, so before the display we buffer the data.
+// https://en.wikipedia.org/wiki/Multiple_buffering
+
+// screen width and height
+Terminal.println("Width: " + g.getWidth())
+Terminal.println("Height: " + g.getHeight())
